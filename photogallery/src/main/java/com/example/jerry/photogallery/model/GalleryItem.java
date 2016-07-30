19af38e2ac7c8209e835860b57abb991edf25780
YuriU <jerryhire8@gmail.com>
2016-07-30 10:50:18 +0300

    gallery example multithreading & images

---
+++ photogallery/src/main/java/com/example/jerry/photogallery/model/GalleryItem.java
@@ -0,0 +1,14 @@
+package com.example.jerry.photogallery.model;
+
+/**
+ * Created by jerry on 26.07.2016.
+ */
+public class GalleryItem {
+    public String Caption;
+    public String Id;
+    public String URL;
+
+    public String toString() {
+        return Caption;
+    }
+}
