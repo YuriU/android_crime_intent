b77dd1bf41ede52a8c541b82c39cead258c4d7e0
YuriU <jerryhire8@gmail.com>
2016-07-31 22:25:27 +0300

    Polling for changes

--- photogallery/src/main/java/com/example/jerry/photogallery/PhotoGalleryActivity.java
+++ photogallery/src/main/java/com/example/jerry/photogallery/PhotoGalleryActivity.java
@@ -1,15 +1,40 @@
 package com.example.jerry.photogallery;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.jerry.common.Activities.SingleFragmentActivity;
+import com.example.jerry.photogallery.model.FlickrFetchr;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    private static final String TAG = "PhotoGalleryActivity";
 
     @Override
     protected Fragment createFragment() {
         return new PhotoGalleryFragment();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+
+        PhotoGalleryFragment fragment = (PhotoGalleryFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentContainer);
+
+        if(Intent.ACTION_SEARCH.equals(intent.getAction())){
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.i(TAG, "Received a new search query: " + query);
+
+            PreferenceManager.getDefaultSharedPreferences(this)
+                    .edit()
+                    .putString(FlickrFetchr.PREF_SEARCH_QUERY, query)
+                    .commit();
+        }
+
+        fragment.updateItems();
     }
 }
--- photogallery/src/main/java/com/example/jerry/photogallery/model/FlickrFetchr.java
+++ photogallery/src/main/java/com/example/jerry/photogallery/model/FlickrFetchr.java
@@ -21,49 +21,50 @@ import java.util.ArrayList;
 public class FlickrFetchr {
     public static final String TAG = "FlickrFetchr";
 
+    public static final String PREF_SEARCH_QUERY = "searchQuery";
+    public static final String PREF_LAST_RESULT_ID = "lastResultId";
+
     private static final String ENDPOINT = "https://api.flickr.com/services/rest/";
     private static final String API_KEY = "9a33b03b3ef9e90053cb7df2412464c2";
     private static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
+    private static final String METHOD_SEARCH = "flickr.photos.search";
     private static final String PARAM_EXTRAS = "extras";
+    private static final String PARAM_TEXT = "text";
     private static final String EXTRA_SMALL_URL = "url_s";
     private static final String XML_PHOTO = "photo";
 
-    byte[] getUrlBytes(String urlSpec) throws IOException {
-        URL url = new URL(urlSpec);
-        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
 
-        try {
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            InputStream in = connection.getInputStream();
-            if(connection.getResponseCode() != HttpURLConnection.HTTP_OK){
-                return null;
-            }
 
-            int bytesRead = 0;
-            byte[] buffer = new byte[1024];
-            while ((bytesRead = in.read(buffer)) > 0) {
-                out.write(buffer, 0, bytesRead);
-            }
-            out.close();
-            return out.toByteArray();
-        } finally {
-            connection.disconnect();
-        }
-    }
 
     public String getUrl(String urlSpec) throws IOException {
         return new String(getUrlBytes(urlSpec));
     }
 
     public ArrayList<GalleryItem> fetchItems(){
+        String url = Uri.parse(ENDPOINT).buildUpon()
+                .appendQueryParameter("method", METHOD_GET_RECENT)
+                .appendQueryParameter("api_key", API_KEY)
+                .appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+                .build().toString();
+
+        return downloadGalleryItems(url);
+    }
+
+    public ArrayList<GalleryItem> search(String query){
+        String url = Uri.parse(ENDPOINT).buildUpon()
+                .appendQueryParameter("method", METHOD_SEARCH)
+                .appendQueryParameter("api_key", API_KEY)
+                .appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+                .appendQueryParameter(PARAM_TEXT, query)
+                .build().toString();
+
+        return downloadGalleryItems(url);
+    }
+
+
+    private ArrayList<GalleryItem> downloadGalleryItems(String url){
         ArrayList<GalleryItem> items = new ArrayList<GalleryItem>();
         try{
-            String url = Uri.parse(ENDPOINT).buildUpon()
-                    .appendQueryParameter("method",METHOD_GET_RECENT)
-                    .appendQueryParameter("api_key",API_KEY)
-                    .appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
-                    .build().toString();
-
             String xmlString = getUrl(url);
             Log.i(TAG, "Received xml:" + xmlString);
             XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
@@ -100,6 +101,29 @@ public class FlickrFetchr {
             }
 
             eventType = parser.next();
+        }
+    }
+
+    byte[] getUrlBytes(String urlSpec) throws IOException {
+        URL url = new URL(urlSpec);
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+            if(connection.getResponseCode() != HttpURLConnection.HTTP_OK){
+                return null;
+            }
+
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, bytesRead);
+            }
+            out.close();
+            return out.toByteArray();
+        } finally {
+            connection.disconnect();
         }
     }
 }
